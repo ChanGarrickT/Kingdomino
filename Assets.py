4e8d0c3caacab83333ebedd4a5955625d4677ae7
Garrick Chan <changarrickt@gmail.com>
2020-12-30 00:00:16 -0800

    Implemented place_domino Added more features to Board to track size

--- Assets.py
+++ Assets.py
@@ -9,20 +9,78 @@ MINE = 'M'
 
 class Board:
     def __init__(self):
+        """Initializes the Board"""
         self._grid = [[None for i in range(9)] for j in range(9)]
         self._grid[4][4] = (CASTLE, 0)
-        self._leftest = 4
-        self._highest = 4
+        self._topmost = 4
+        self._bottommost = 4
+        self._leftmost = 4
+        self._rightmost = 4
 
     def get_grid(self):
+        """Returns the 2D list representation of the Board"""
         return self._grid
 
+    def get_max_size(self):
+        """Returns the maximum width of terrain-containing tiles"""
+        return (len(self._grid) / 2) + 1
+
     def get_coord(self, row, col):
+        """
+        Returns the item at a particular coordinate, or None if the coordinates are out of bounds
+        :param row: the row to check - cannot be negative
+        :param col: the column to check - cannot be negative
+        :return: the item at (row, col)
+        """
         if 0 <= row < len(self._grid) and 0 <= col < len(self._grid[0]):
             return self._grid[row][col]
         else:
             return None
+    
+    def get_topmost(self):
+        """Returns the lowest index row which contains an object"""
+        return self._topmost
+    
+    def set_topmost(self, row):
+        """
+        Update the lowest index row which contains an object
+        :param row: the index of the new topmost row
+        """
+        self._topmost = row
 
+    def get_bottommost(self):
+        """Returns the greatest index row which contains an object"""
+        return self._bottommost
+
+    def set_bottommost(self, row):
+        """
+        Update the greatest index row which contains an object
+        :param row: the index of the new bottommost row
+        """
+        self._bottommost = row
+        
+    def get_leftmost(self):
+        """Returns the lowest index column with contains an object"""
+        return self._leftmost
+    
+    def set_leftmost(self, col):
+        """
+        Update the lowest index column which contains an object
+        :param col: the index of the new leftmost column
+        """
+        self._leftmost = col
+
+    def get_rightmost(self):
+        """Returns the greatest index column with contains an object"""
+        return self._rightmost
+
+    def set_rightmost(self, col):
+        """
+        Update the greatest index column which contains an object
+        :param col: the index of the new rightmost column
+        """
+        self._rightmost = col
+    
     def check_match(self, row, col, a_terrain):
         """
         Checks if the terrain at a coordinate matches the parameter terrain
@@ -104,6 +162,3 @@ class Player:
 
     def get_board(self):
         return self._board
-
-b = Board()
-b.print_board()
--- Engine.py
+++ Engine.py
@@ -1,6 +1,7 @@
 from Assets import Player
 import json
 import random
+from copy import deepcopy
 
 P1 = 0
 P2 = 1
@@ -51,30 +52,35 @@ class Engine:
             print('Player names not unique')
 
     def get_game_over(self):
-        """
-        Returns whether the game is over
-        :return: True if all tiles have been claimed
-        """
+        """Returns whether the game is over"""
         if not self._valid_setup:
             print('Invalid game setup')
             return
         return self._game_over
 
     def get_turn(self):
-        """
-        Returns the Player whose turn it is
-        :return: the Player
-        """
+        """Returns the Player whose turn it is"""
         if not self._valid_setup:
             print('Invalid game setup')
             return
         return self._turn_order[self._turn]
 
+    def get_player(self, name):
+        """
+        Returns a player instance given a name
+        :param name: the name of the player to search for
+        :return: the player instance, or None if name not found
+        """
+        if not self._valid_setup:
+            print('Invalid game setup')
+            return
+        try:
+            return self._players[name]
+        except KeyError:
+            print('Name not found')
+
     def get_player_count(self):
-        """
-        Returns the number of players in the game
-        :return: the number of players
-        """
+        """Returns the number of players in the game"""
         if not self._valid_setup:
             print('Invalid game setup')
             return
@@ -124,7 +130,7 @@ class Engine:
             self._turn += 1
             print(name + ' claimed domino ' + str(self._deal[position]) + ' from slot ' + str(position + 1))
             # Once all tiles are claimed, move to placement phase
-            if self._turn == len(self._players):
+            if self._turn >= len(self._players):
                 self._turn = 0
                 self._phase = PLACE
                 print('Moving to Placement phase')
@@ -142,54 +148,61 @@ class Engine:
             print('Invalid game setup')
             return
         if self._phase != PLACE:
-            print('Not currently place phase')
+            print('Not currently Placement phase')
             return
         if not validate_coord(coord1, coord2):
             print('Invalid coordinates')
             return
         try:
             player = self._players[name]
+            if player.get_dom_on_hold() is None:
+                print(name + ' has no domino on hold')
+                return
+            if not validate_overlap(player.get_board(), coord1, coord2):
+                print('One or more coordinates overlap with existing tile')
+                return
             if not validate_neighbor(player.get_board(), player.get_dom_on_hold(), coord1, coord2):
                 print('No matching adjacent tile')
-            else:
-                pass
+                return
+            max_size = player.get_board().get_max_size()
+            size_updates = validate_size(player.get_board(), coord1, coord2, max_size)
+            if not size_updates:
+                print('Terrain grid cannot exceed ' + max_size + 'x' + max_size)
+                return
+            player.get_board().set_topmost(size_updates[0])
+            player.get_board().set_bottommost(size_updates[1])
+            player.get_board().set_leftmost(size_updates[2])
+            player.get_board().set_rightmost(size_updates[3])
+            player.get_board().set_coord(coord1[0], coord1[1], player.get_dom_on_hold()[0])
+            player.get_board().set_coord(coord2[0], coord2[1], player.get_dom_on_hold()[1])
+            print(name + ' placed ' + str(player.get_dom_on_hold()[0]) + ' at ' + str(coord1) +
+                            ' and ' + str(player.get_dom_on_hold()[1]) + ' at ' + str(coord2))
+            player.set_dom_on_hold(None)
+            self._turn += 1
+            if self._turn >= len(self._players):
+                self._turn = 0
+                self._phase = CLAIM
+                print('Moving to Claim phase')
         except KeyError:
             print("Name not found")
 
-
-def validate_overlap(board, coord1, coord2):
-    pass
-
-
-def validate_neighbor(board, domino, coord1, coord2):
-    """
-    Checks whether a new domino would be adjacent to a matching terrain
-    :param board: the board to check
-    :param domino: a tuple of tuples representing the domino's tiles
-    :param coord1: a tuple (row, col) representing where to place tile1 of the domino
-    :param coord2: a tuple (row, col) representing where to place tile2 of the domino
-    :return: True if the domino can be placed at the given coordinate, False if not
-    """
-    x1, y1, x2, y2 = coord1[0], coord1[1], coord2[0], coord2[1]
-    # Check if either desired destination is already occupied
-    if board.get_coord(x1, y1) is not None or board.get_coord(x2, y2):
-        return False
-    # Check if tiles adjacent to the destination are compatible
-    terrain1 = domino[0][0]
-    terrain2 = domino[1][0]
-    validity1 = (board.check_match_castle(x1 - 1, y1, terrain1) or
-                 board.check_match_castle(x1 + 1, y1, terrain1) or
-                 board.check_match_castle(x1, y1 - 1, terrain1) or
-                 board.check_match_castle(x1, y1 + 1, terrain1))
-    validity2 = (board.check_match_castle(x2 - 1, y2, terrain2) or
-                 board.check_match_castle(x2 + 1, y2, terrain2) or
-                 board.check_match_castle(x2, y2 - 1, terrain2) or
-                 board.check_match_castle(x2, y2 + 1, terrain2))
-    return validity1 or validity2
-
-
-def validate_size(board, coord1, coord2):
-    pass
+    def discard_domino(self, name):
+        if not self._valid_setup:
+            print('Invalid game setup')
+            return
+        if self._phase != PLACE:
+            print('Not currently Placement phase')
+            return
+        try:
+            player = self._players[name]
+            player.set_dom_on_hold(None)
+            self._turn += 1
+            if self._turn >= len(self._players):
+                self._turn = 0
+                self._phase = CLAIM
+                print('Moving to Claim phase')
+        except KeyError:
+            print('Name not found')
 
 
 def validate_coord(coord1, coord2):
@@ -209,6 +222,62 @@ def validate_coord(coord1, coord2):
         return False
 
 
+def validate_overlap(board, coord1, coord2):
+    """
+    Checks whether the new domino would overlap with existing terrain
+    :param board: the board to check
+    :param coord1: a tuple (row, col) representing where to place tile1 of the domino
+    :param coord2: a tuple (row, col) representing where to place tile2 of the domino
+    :return: True if both coordinates are empty, False otherwise
+    """
+    x1, y1, x2, y2 = coord1[0], coord1[1], coord2[0], coord2[1]
+    return board.get_coord(x1, y1) is None and board.get_coord(x2, y2) is None
+
+
+def validate_neighbor(board, domino, coord1, coord2):
+    """
+    Checks whether a new domino would be adjacent to a matching terrain
+    :param board: the board to check
+    :param domino: a tuple of tuples representing the domino's tiles
+    :param coord1: a tuple (row, col) representing where to place tile1 of the domino
+    :param coord2: a tuple (row, col) representing where to place tile2 of the domino
+    :return: True if the domino can be placed at the given coordinate, False if not
+    """
+    x1, y1, x2, y2 = coord1[0], coord1[1], coord2[0], coord2[1]
+    terrain1 = domino[0][0]
+    terrain2 = domino[1][0]
+    validity1 = (board.check_match_castle(x1 - 1, y1, terrain1) or
+                 board.check_match_castle(x1 + 1, y1, terrain1) or
+                 board.check_match_castle(x1, y1 - 1, terrain1) or
+                 board.check_match_castle(x1, y1 + 1, terrain1))
+    validity2 = (board.check_match_castle(x2 - 1, y2, terrain2) or
+                 board.check_match_castle(x2 + 1, y2, terrain2) or
+                 board.check_match_castle(x2, y2 - 1, terrain2) or
+                 board.check_match_castle(x2, y2 + 1, terrain2))
+    return validity1 or validity2
+
+
+def validate_size(board, coord1, coord2, max_size):
+    """
+    Checks whether placing the new domino would result in the board exceeding the maximum size
+    :param board: the board to check
+    :param coord1: a tuple (row, col) representing where to place tile1 of the domino
+    :param coord2: a tuple (row, col) representing where to place tile2 of the domino
+    :param max_size: the maximum number of tiles wide the board can be
+    :return: a list of board bounds to update if the resulting board is within bounds, False otherwise
+    """
+    dup_board = deepcopy(board)
+    x1, y1, x2, y2 = coord1[0], coord1[1], coord2[0], coord2[1]
+    topmost = min(board.get_topmost(), x1, x2)
+    bottommost = max(board.get_bottommost(), x1, x2)
+    leftmost = min(board.get_leftmost(), y1, y2)
+    rightmost = max(board.get_rightmost(), y1, y2)
+    if bottommost - topmost > max_size or rightmost - leftmost > max_size:
+        return False
+    else:
+        return [topmost, bottommost, leftmost, rightmost]
+
+
 def score_board(board):
     pass
 
@@ -218,3 +287,7 @@ e = Engine(['blue', 'pink'], True)
 e.deal_dominoes()
 e.claim_domino('blue', 0)
 e.claim_domino('pink', 1)
+e.place_domino('blue', (3,4), (3,5))
+e.get_player('blue').get_board().print_board()
+e.place_domino('pink', (4,6), (4,7))
+e.get_player('pink').get_board().print_board()
